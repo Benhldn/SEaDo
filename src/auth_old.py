987dc1960d6aed1764f3840ac959689826434f2e
BH <ben.hldn2022@gmail.com>
2025-03-17 09:25:58 +0000

    hashed pw

--- src/auth_old.py
+++ src/auth_old.py
@@ -3,7 +3,6 @@ from .models import User
 from .database import db_session
 from flask_login import login_user, login_required, logout_user
 from sqlalchemy.exc import NoResultFound
-from werkzeug.security import generate_password_hash, check_password_hash
 
 auth = Blueprint('auth', __name__)
 
@@ -13,55 +12,65 @@ def login():
 
 @auth.route('/login', methods=['POST'])
 def loginPost():
+
     email = request.form.get('inputEmail')
     password = request.form.get('inputPassword')
     
+    print(email, password)
+
     try:
         user = db_session.query(User).filter(User.email == email).one()
     except NoResultFound:
-        flash('Invalid email or password. Please try again.')
         return redirect(url_for('auth.login'))
+    
+    print(user)
 
-    # Verify the hashed password
-    if not check_password_hash(user.password, password):
-        flash('Invalid email or password. Please try again.')
+    actualPassword = db_session.query(User.password).filter_by(email=email).first()[0]
+    if password != actualPassword:
         return redirect(url_for('auth.login'))
     else:
         login_user(user)
         return redirect(url_for('main.home'))
+    
+
 
 @auth.route('/success')
 @login_required
 def success():
     return render_template('home.html')
 
+    
 @auth.route('/signUp')
 def signUp():
     return render_template('signUp.html')
 
+
 @auth.route('/signUp', methods=['GET', 'POST'])
 def signUpPost():
+
     email = request.form.get('inputEmail')
     password = request.form.get('inputPassword')
 
-    user = db_session.query(User).filter(User.email == email).one_or_none()
+    user = db_session.query(User).filter(User.email == email).one_or_none() 
 
     if user:
         flash('User already exists, try again.')
         return redirect(url_for('auth.signUp'))
     else:
-        # Hash the password
-        hashed_password = generate_password_hash(password, method='sha256')
-        
-        new_user = User(email=email, password=hashed_password)
+
+        new_user = User(email=email, password=password)
 
         db_session.add(new_user)
         db_session.commit()
-        flash('Account created successfully!')
+        print(new_user)
 
     return redirect(url_for('auth.login'))
+
 
 @auth.route("/logout")
 def logout():
     logout_user()
     return redirect(url_for("auth.login"))
+
+
+
